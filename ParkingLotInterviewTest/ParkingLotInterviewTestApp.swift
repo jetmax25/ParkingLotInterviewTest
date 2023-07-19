7634a15d6092e87d4f37c30f85d64d014b869d72
Mike <jetmax25@gmail.com>
2023-07-19 13:20:41 -0400

    Initial Commit

---
+++ ParkingLotInterviewTest/ParkingLotInterviewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingLotInterviewTestApp.swift
+//  ParkingLotInterviewTest
+//
+//  Created by Michael Isasi on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ParkingLotInterviewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
